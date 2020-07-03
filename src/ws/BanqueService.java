e8550357d0a4b2497ef17d0ff1b3b95dc1b2563c
saci zakaria <sacideveloppeur@gmail.com>
2020-07-03 10:28:37 +0200

    Annotation: 0WebService/@WebMethod/@WebParam

--- src/ws/BanqueService.java
+++ src/ws/BanqueService.java
@@ -4,32 +4,34 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.jws.WebService;
+
 import metier.Compte;
 
-
-
-
 /**
- * @author Zakaria
- * POJO PLain Old Java Project
+ * @author Zakaria POJO PLain Old Java Project
  */
+
+@WebService(serviceName = "BanqueWS")
 public class BanqueService {
-public double conversion(double mt) {
-	return mt*11;
-}
+	@WebMethod(operationName = "ConversionEuroToDH")
+	public double conversion(@WebParam(name = "monton")double mt) {
+		return mt * 11;
+	}
+	@WebMethod(operationName = "getCompte")
+	public Compte getCompte(@WebParam(name = "code")int code) {
+		return new Compte(code, Math.random() * 9000, new Date());
+	}
+	@WebMethod(operationName = "listComptes")
+	public List<Compte> listComptes() {
+		List<Compte> comptes = new ArrayList<>();
+		comptes.add(new Compte(1, Math.random() * 9000, new Date()));
+		comptes.add(new Compte(2, Math.random() * 9000, new Date()));
+		comptes.add(new Compte(3, Math.random() * 9000, new Date()));
+		return comptes;
 
-public Compte getCompte(int code) {	
-	return new Compte(code,Math.random()*9000,new Date());
-}
-public List<Compte> listComptes() {
-	List<Compte> comptes=new ArrayList<>();
-	comptes.add(new Compte(1,Math.random()*9000,new Date()));	
-	comptes.add(new Compte(2,Math.random()*9000,new Date()));
-	comptes.add(new Compte(3,Math.random()*9000,new Date()));
-	return comptes;
-	
-}
-
-
+	}
 
 }
